7409aae5803aed10d65e4582a396f1f27e94f4b4
alfa <01alfa.cool@gmail.com>
2024-12-20 21:48:16 +0700

    fix(currency): fix responsive field

--- app/(main)/currency/components/AmountInput.tsx
+++ app/(main)/currency/components/AmountInput.tsx
@@ -3,7 +3,7 @@ import { InputNumber } from 'primereact/inputnumber';
 
 export function AmountInput({ amount, onChange }) {
     return (
-        <div className="field col">
+        <div className="field col-12 lg:col-4">
             <label htmlFor="AmountInput">Amount</label>
             <InputNumber inputId="AmountInput" value={amount} onValueChange={onChange} minFractionDigits={0} maxFractionDigits={100} />
         </div>
--- app/(main)/currency/components/CodeSelector.tsx
+++ app/(main)/currency/components/CodeSelector.tsx
@@ -3,7 +3,7 @@ import { Dropdown } from 'primereact/dropdown';
 
 export function CodeSelector({ label, val, opt, onChange }) {
     return (
-        <div className="field col">
+        <div className="field col-12 lg:col-4">
             <label htmlFor={label}>{label}</label>
             <Dropdown filter id={label} value={val} onChange={onChange} options={opt} optionLabel="full_name" placeholder="Select Country" />
         </div>
